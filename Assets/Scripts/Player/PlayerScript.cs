ed551ad78741200fdf2e14e2097fdc2a2180759d
Michael Butkevicius <mjbutkevicius@gmail.com>
2024-02-22 22:39:45 -0600

    prevented multijumping

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Callbacks;
@@ -8,11 +9,15 @@ public class playerScript : MonoBehaviour
     public Rigidbody2D playerRigidBody;
     private float playerSpeed = 8f;
     private float playerJump = 8f;
+    private bool isGrounded;
+    private bool isTouchingWallRight;
+    private bool isTouchingWallLeft;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log("start");
     }
 
     // Update is called once per frame
@@ -23,9 +28,46 @@ public class playerScript : MonoBehaviour
         playerRigidBody.velocity = new Vector2(moveInput * playerSpeed, playerRigidBody.velocity.y);
 
         // Jumping
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, playerJump);
         }        
+    }
+
+        void FixedUpdate()
+    {
+        // Horizontal movement only when grounded
+        if (isGrounded)
+        {
+            float moveInput = Input.GetAxisRaw("Horizontal");
+            playerRigidBody.velocity = new Vector2(moveInput * playerSpeed, playerRigidBody.velocity.y);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        // Check if the player is grounded
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            if (Vector2.Dot(contact.normal, Vector2.up) > 0.5)
+            {
+                isGrounded = true;
+                return;
+            }
+            else if (contact.normal.x > 0.5)
+            {
+                isTouchingWallRight = true;
+                Debug.Log(isTouchingWallRight);
+            }
+            else if (contact.normal.x < -0.5)
+            {
+                isTouchingWallLeft = true;
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 }
