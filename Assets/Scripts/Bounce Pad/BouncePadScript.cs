eac060068d6f40c881b9a4f766037c66b64593cb
Michael Butkevicius <mjbutkevicius@gmail.com>
2024-02-23 22:18:47 -0600

    tweaked player movement. Added increased gravitytowards peak of player height

---
+++ Assets/Scripts/Bounce Pad/BouncePadScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePadScript : MonoBehaviour
+{
+    public BoxCollider2D platformCollider;
+
+    void Start()
+    {
+        if (platformCollider == null)
+        {
+            Debug.LogError("Platform Collider is missing!");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        // Disable collision with player when entering from below
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Touched sprite");
+            other.GetComponent<PlayerScript>().BounceJump();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // Enable collision with player when leaving the platform
+        if (other.CompareTag("Player"))
+        {
+            Physics2D.IgnoreCollision(other, platformCollider, false);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -4,46 +4,69 @@ using System.Collections.Generic;
 using UnityEditor.Callbacks;
 using UnityEngine;
 
-public class playerScript : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
     public Rigidbody2D playerRigidBody;
-    private float playerSpeed = 8f;
-    private float playerJump = 8f;
+
+    public float playerSpeed = 7f;
+    public float playerJump = 12.5f;
+    public float baseGravity = 3;
+    public float airGravity = 4;
+    public float bounceJump = 2;
+    // the higher the number, the faster the increased gravity will hit the player at the peak of the jump
+    public float ApproachingPeakJump = 5;
+    private float previousVelocityY;
     private bool isGrounded;
     private bool isTouchingWallRight;
     private bool isTouchingWallLeft;
+    private bool isApproachingPeak = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("start");
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        // get the direction player is moving (right key=1 leftkey=-1 no key=0)
         float moveInput = Input.GetAxisRaw("Horizontal");
+        // multiply direction by player speed and preserve vertical velocity
         playerRigidBody.velocity = new Vector2(moveInput * playerSpeed, playerRigidBody.velocity.y);
 
-        // Jumping
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, playerJump);
-        }        
-    }
-
-        void FixedUpdate()
-    {
-        // Horizontal movement only when grounded
-        if (isGrounded)
-        {
-            float moveInput = Input.GetAxisRaw("Horizontal");
-            playerRigidBody.velocity = new Vector2(moveInput * playerSpeed, playerRigidBody.velocity.y);
+        // Jump if player presses spacebar
+        Jump();
+ 
+        // if player is grounded and velocity == 0, return player to regular gravity bounds
+        if (playerRigidBody.velocity.y == 0 && isGrounded){
+            playerRigidBody.gravityScale = baseGravity;
         }
+
+        // checks if velocity is approaching increased gravity levels
+        // TODO: gravity stays high if continuously bouncing on bounce pad. Need to check if player bounced to determine 
+        // if gravity needs to go back to default
+        if (previousVelocityY > ApproachingPeakJump && playerRigidBody.velocity.y <= ApproachingPeakJump)
+        {
+            isApproachingPeak = true;
+        }
+        // if velocity is approaching jump height peak, increase gravity 
+        if  (isApproachingPeak){
+            playerRigidBody.gravityScale = airGravity;
+        }
+        // continuously track previous vertical position
+        previousVelocityY = playerRigidBody.velocity.y;
+        isApproachingPeak = false;
     }
 
+    void FixedUpdate()
+    {
+
+    }
+
+    // TODO: work on making sure player doesn't stick to wall. This function is still buggy
     void OnCollisionStay2D(Collision2D collision)
     {
         // Check if the player is grounded
@@ -66,8 +89,25 @@ public class playerScript : MonoBehaviour
         }
     }
 
+    // same here, still buggy
     void OnCollisionExit2D(Collision2D collision)
     {
         isGrounded = false;
+    }
+
+    // allows the player to jump 
+    public void Jump(){
+        // get the player 
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, playerJump);  
+    }
+
+    // determines player jump height when bouncing from jump pad
+    public void BounceJump(){
+        // get the player horizontal direction
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        // preserve horizontal velocity and uses bounceJump to make player go extra high
+        playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, playerJump * bounceJump);
     }
 }
