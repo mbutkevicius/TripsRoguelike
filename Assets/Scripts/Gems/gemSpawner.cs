1178608c983986d937f88130acc882b8f0e7e062
William Butkevicius <wbutkevicius@gmail.com>
2024-02-25 23:07:55 -0600

    Added a tilemap template prefab, gave gems triggers and an add score functionality, made a global logic manager that will store high-level info such as player score, added folders for sounds

--- Assets/Scripts/Gems/gemSpawner.cs
+++ Assets/Scripts/Gems/gemSpawner.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class gemSpawner : MonoBehaviour
 {
+    [System.Serializable]
+    public class gemSpawns
+    {
+        public GameObject gemSpawnPoint;
+        public bool spawnIsOccupied;
+    }
+
+    public List<gemSpawns> gameObjectList = new List<gemSpawns>();
+
+
+
+
+
+    public bool GameIsActive = true;
+
+    // Spawning timer
+    public float GemSpawningTimer;
+
+    // Lists
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // StartCoroutine(GemSpawningSystem());
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
+    // Method to select a GameObject and change its corresponding bool
+    public void SelectGameObjectAndChangeBool(GameObject selectedObject, bool newBoolValue)
+    {
+        // Iterate through the list
+        foreach (gemSpawns item in gameObjectList)
+        {
+            // Check if the GameObject matches the selectedObject
+            if (item.gemSpawnPoint == selectedObject)
+            {
+                // Update the bool value
+                item.spawnIsOccupied = newBoolValue;
+                break; // Exit the loop since we found the matching GameObject
+            }
+        }
+    }
+
 }
